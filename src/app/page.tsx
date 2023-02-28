006877fb647a3e21381705a708dd3a62a28fc134
Kobe De Peuter <kobedepeuter@outlook.com>
2023-02-28 21:05:27 +0100

    Write a small usage part.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,25 +3,16 @@ import ErrorCodes from "@/components/documentation/ErrorCodes";
 import CommonQueryParams from "@/components/documentation/CommonQueryParams";
 import Limits from "@/components/documentation/Limits";
 import AccessLevels from "@/components/documentation/AccessLevels";
-
-// FIXME 2023/02/12 - Implement documentation page.
+import Usage from "@/components/documentation/Usage";
 
 // noinspection JSUnusedGlobalSymbols
 export default function Home() {
     return (
         <div className="space-y-2 mb-10">
-            <ul>
-                <li>TODO: Introduction</li>
-            </ul>
-
             <section>
                 <DocumentationTitle id="usage">Usage</DocumentationTitle>
 
-                <ul>
-                    <li>TODO: Acceptable usage</li>
-                    <li>TODO: Selection usage + combining</li>
-                    <li>TODO: id</li>
-                </ul>
+                <Usage />
             </section>
             <section>
                 <DocumentationTitle id="query">Common Query Parameters</DocumentationTitle>
@@ -40,10 +31,6 @@ export default function Home() {
             </section>
             <section>
                 <DocumentationTitle id="errors">Error Codes</DocumentationTitle>
-
-                <ul>
-                    <li>TODO: Error format</li>
-                </ul>
 
                 <ErrorCodes />
             </section>
