9cd6e636fbfc54506fe4b0842f1b7387536e7d63
Kobe De Peuter <kobedepeuter@outlook.com>
2023-03-05 19:19:24 +0100

    Remove the border when there are no calls yet.

--- src/components/try-it/call-response/CallResponse.tsx
+++ src/components/try-it/call-response/CallResponse.tsx
@@ -8,21 +8,24 @@ export default function CallResponse() {
     return (
         <div>
             <h3 className="text-xl font-bold capitalize mb-2">Responses</h3>
-            <div className="rounded-lg border-base-300 border-4">
-                {calls.responses.length
-                    ? calls.responses
-                          .sort((a, b) => b.timestamp - a.timestamp)
-                          .map((response) => (
-                              <label key={response.timestamp} className="collapse collapse-arrow odd:bg-base-300 even:bg-base-200">
-                                  <input type="checkbox" />
-                                  <div className="collapse-title text-xl font-medium">{response.url}</div>
-                                  <div className="collapse-content prose max-w-none">
-                                      <pre>{JSON.stringify(response.data, null, 4)}</pre>
-                                  </div>
-                              </label>
-                          ))
-                    : "You haven't made any calls yet."}
-            </div>
+
+            {calls.responses.length ? (
+                <div className="rounded-lg border-base-300 border-4">
+                    {calls.responses
+                        .sort((a, b) => b.timestamp - a.timestamp)
+                        .map((response) => (
+                            <label key={response.timestamp} className="collapse collapse-arrow odd:bg-base-300 even:bg-base-200">
+                                <input type="checkbox" />
+                                <div className="collapse-title text-xl font-medium">{response.url}</div>
+                                <div className="collapse-content prose max-w-none">
+                                    <pre>{JSON.stringify(response.data, null, 4)}</pre>
+                                </div>
+                            </label>
+                        ))}
+                </div>
+            ) : (
+                "You haven't made any calls yet."
+            )}
         </div>
     );
 }
