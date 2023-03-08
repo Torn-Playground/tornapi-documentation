8dfa1d9b22c20ad74c2a0a977c379ff36d8a8a22
Kobe De Peuter <kobedepeuter@outlook.com>
2023-03-08 20:18:12 +0100

    Document the cache. (#18)

--- src/components/documentation/Limits.tsx
+++ src/components/documentation/Limits.tsx
@@ -29,6 +29,10 @@ export default function Limits() {
             Some data is stored in the cloud. Endpoints that pull that data have an additional limit of 100,000 rows a day, for a specific category. Looking at
             the same data in-game will also count towards the same limit, going over the limit will block access through both the api and in-game. The different
             categories are faction news, activity log and personal stats.
+            <LimitTitle id="cache">Cache</LimitTitle>
+            Calls to the API are cached. This cache lasts 29 seconds, so you can space your calls 30 seconds between them. Changing the section, selection or id
+            will have a different cache, but other parameters like to and from won&apos;t. Like always, there are exceptions to this which will be documented on
+            the specific selections.
         </>
     );
 }
