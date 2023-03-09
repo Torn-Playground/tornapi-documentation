386a2ea164e1c80a171228224de50961bc2706bb
Kobe De Peuter <kobedepeuter@outlook.com>
2023-03-09 01:21:01 +0100

    Map property selections. (#21)

--- src/api-schema/common-types.ts
+++ src/api-schema/common-types.ts
@@ -6,3 +6,6 @@ export const Number = "Number (with floating point)";
 export const NumberBoolean = "NumberBoolean (0 for false, 1 for true)";
 export const ArrayString = "Array of strings";
 export const EpochSeconds = "Epoch timestamp (in seconds)";
+
+// TODO 2023/03/08 - Handle bug report types.
+export const BugReportPending = "Bug report pending, type uncertain.";
---
+++ src/api-schema/property/property.ts
@@ -0,0 +1,36 @@
+import { fromStructure, Schema, Selection, Structure } from "@/api-schema/schema.types";
+import { ArrayString, BugReportPending, Integer } from "@/api-schema/common-types";
+
+const propertyStructure: Structure = {
+    id: "property",
+    name: "Property",
+    schema: {
+        owner_id: { type: Integer },
+        property_type: { type: Integer },
+        happy: { type: Integer },
+        upkeep: { type: Integer },
+        upgrades: { type: ArrayString },
+        staff: { type: ArrayString },
+        rented: { type: BugReportPending },
+        users_living: { type: BugReportPending },
+    },
+};
+const structures = [propertyStructure];
+
+const schema: Schema = {
+    property: fromStructure(propertyStructure),
+};
+
+const PropertySelection: Selection = {
+    section: "property",
+    name: "property",
+    description: "View a specific ranked war report.",
+    access: "public",
+    schema,
+    structures,
+    id: {
+        optional: true,
+    },
+};
+
+export default PropertySelection;
