ff9527c93ddf6e4dce09548f10191fcb49841b6d
Kobe De Peuter <kobedepeuter@outlook.com>
2023-03-09 22:43:30 +0100

    Make sure the alert doesn't go out of the window on desktop.

--- src/components/global/community-alert/CommunityAlert.tsx
+++ src/components/global/community-alert/CommunityAlert.tsx
@@ -23,7 +23,7 @@ export default function CommunityAlert() {
     }
 
     return (
-        <div className="fixed bottom-10 z-40 flex items-center w-full pl-3 pr-8">
+        <div className="fixed bottom-10 z-40 flex items-center w-full px-3">
             <div className="alert alert-info shadow-lg">
                 <div>
                     <InfoIcon size={24} />
