a24c22cf325b1d5af2f924313ab8126566a6fe0c
Kobe De Peuter <kobedepeuter@outlook.com>
2023-03-11 14:50:17 +0100

    Update property mapping based on the B&I outcome. (#37)

--- src/api-schema/property/property.ts
+++ src/api-schema/property/property.ts
@@ -1,6 +1,16 @@
 import { fromStructure, Schema, Selection, Structure } from "@/api-schema/schema.types";
-import { ArrayString, BugReportPending, Integer } from "@/api-schema/common-types";
+import { ArrayString, Integer, IntegerAndString } from "@/api-schema/common-types";
 
+const rentedStructure: Structure = {
+    id: "rented",
+    name: "Rented",
+    schema: {
+        user_id: { type: Integer },
+        days_left: { type: Integer },
+        total_cost: { type: Integer },
+        cost_per_day: { type: Integer },
+    },
+};
 const propertyStructure: Structure = {
     id: "property",
     name: "Property",
@@ -11,11 +21,11 @@ const propertyStructure: Structure = {
         upkeep: { type: Integer },
         upgrades: { type: ArrayString },
         staff: { type: ArrayString },
-        rented: { type: BugReportPending },
-        users_living: { type: BugReportPending },
+        rented: fromStructure(rentedStructure, { nullable: true, array: false }),
+        users_living: { type: IntegerAndString, nullable: true },
     },
 };
-const structures = [propertyStructure];
+const structures = [propertyStructure, rentedStructure];
 
 const schema: Schema = {
     property: fromStructure(propertyStructure),
