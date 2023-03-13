1d960e8435ea830093a012dae14294a853d785b5
Kobe De Peuter <kobedepeuter@outlook.com>
2023-03-13 21:50:14 +0100

    Include all faction selections as comment.

--- src/api-schema/faction/index.ts
+++ src/api-schema/faction/index.ts
@@ -1,7 +1,42 @@
 import { Section, Selection } from "@/api-schema/schema.types";
 import { LookupSelection, TimestampSelection } from "@/api-schema/common-selections";
 
-const selections: Selection[] = [LookupSelection, TimestampSelection];
+const selections: Selection[] = [
+    //  applications
+    //  armor
+    //  armorynews
+    //  attacknews
+    //  attacks
+    //  attacksfull
+    //  basic
+    //  boosters
+    //  cesium
+    //  chain
+    //  chainreport
+    //  chains
+    //  contributors
+    //  crimenews
+    //  crimes
+    //  currency
+    //  donations
+    //  drugs
+    //  fundsnews
+    LookupSelection,
+    //  mainnews
+    //  medical
+    //  membershipnews
+    //  positions
+    //  reports
+    //  revives
+    //  revivesfull
+    //  stats
+    //  temporary
+    //  territory
+    //  territorynews
+    TimestampSelection,
+    //  upgrades
+    //  weapons
+];
 
 const FactionSection: Section = {
     selections,
