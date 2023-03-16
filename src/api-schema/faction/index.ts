430026455beaea66ec33045f1af39d807523a0d5
Kobe De Peuter <kobedepeuter@outlook.com>
2023-03-16 12:35:07 +0100

    Map faction/donations.

--- src/api-schema/faction/index.ts
+++ src/api-schema/faction/index.ts
@@ -13,6 +13,7 @@ import ContributorsSelection from "@/api-schema/faction/contributors";
 import CrimeNewsSelection from "@/api-schema/faction/crimenews";
 import CrimesSelection from "@/api-schema/faction/crimes";
 import CurrencySelection from "@/api-schema/faction/currency";
+import DonationsSelection from "@/api-schema/faction/donations";
 import DrugsSelection from "@/api-schema/faction/drugs";
 import FundsNewsSelection from "@/api-schema/faction/fundsnews";
 import MainNewsSelection from "@/api-schema/faction/mainnews";
@@ -43,7 +44,7 @@ const selections: Selection[] = [
     CrimeNewsSelection,
     CrimesSelection,
     CurrencySelection,
-    //  donations
+    DonationsSelection,
     DrugsSelection,
     FundsNewsSelection,
     LookupSelection,
