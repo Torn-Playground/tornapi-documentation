6eee30e2cf7770a03b8c156b250011291c2e0c22
Kobe De Peuter <kobedepeuter@outlook.com>
2023-04-09 14:05:31 +0200

    Map user/hof.

--- src/api-schema/user/index.ts
+++ src/api-schema/user/index.ts
@@ -14,6 +14,7 @@ import DisplaySelection from "@/api-schema/user/display";
 import EducationSelection from "@/api-schema/user/education";
 import EventsSelection from "@/api-schema/user/events";
 import GymSelection from "@/api-schema/user/gym";
+import HOFSelection from "@/api-schema/user/hof";
 import ProfileSelection from "@/api-schema/user/profile";
 
 const selections: Selection[] = [
@@ -31,7 +32,7 @@ const selections: Selection[] = [
     EducationSelection,
     EventsSelection,
     GymSelection,
-    //  hof
+    HOFSelection,
     //  honors
     //  icons
     //  inventory
