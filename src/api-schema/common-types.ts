b14b6d76fa2d326dafb93621171d977bf07272b3
Kobe De Peuter <kobedepeuter@outlook.com>
2023-04-09 14:18:31 +0200

    Map user/honors.

--- src/api-schema/common-types.ts
+++ src/api-schema/common-types.ts
@@ -8,6 +8,7 @@ export const NumberBoolean = "NumberBoolean (0 for false, 1 for true)";
 export const Boolean = "Boolean";
 export const ArrayString = "Array of strings";
 export const ArrayInteger = "Array of integers";
+export const ArrayEpochSeconds = "Array of Epoch timestamp (in seconds)";
 export const EpochSeconds = "Epoch timestamp (in seconds)";
 
 // TODO 2023/03/08 - Handle bug report types.
