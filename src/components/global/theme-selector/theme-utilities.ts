5e719bfd2f561c4921685da5a52650fd8cf9d73a
Kobe De Peuter <kobedepeuter@outlook.com>
2023-04-23 18:42:00 +0200

    Remove unused code.

--- src/components/global/theme-selector/theme-utilities.ts
+++ src/components/global/theme-selector/theme-utilities.ts
@@ -6,13 +6,3 @@ export const THEME_LIGHT: Theme = "autumn";
 export const ALL_THEMES = [THEME_DARK, THEME_LIGHT];
 
 export const STORAGE_KEY = "tornapi-theme";
-
-export const hasMediaSupport = () => typeof window !== "undefined" && typeof window.matchMedia !== "undefined";
-
-export function getDefaultTheme(): Theme {
-    if (!hasMediaSupport()) {
-        return DEFAULT_THEME;
-    }
-
-    return window.matchMedia("(prefers-color-scheme: dark)").matches ? THEME_DARK : THEME_LIGHT;
-}
