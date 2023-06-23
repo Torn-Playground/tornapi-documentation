674b4fc3dc6339f4515f11d38d539a056ed56365
DeKleineKobini <kobedepeuter@outlook.com>
2023-06-23 18:39:34 +0200

    Change mappings to be up-to-date after crimes 2.0 released.

--- src/api-schema/user/skills.ts
+++ src/api-schema/user/skills.ts
@@ -4,6 +4,7 @@ import { Schema, Selection } from "@/api-schema/schema.types";
 const schema: Schema = {
     reviving: { type: String, extra: "in Number format, but it's a string" },
     hunting: { type: String, extra: "in Number format, but it's a string" },
+    search_for_cash: { type: String, nullable: true, extra: "in Number format, but it's a string; Only present for players that migrated to crimes 2.0." },
     racing: { type: String, extra: "in Number format, but it's a string" },
     player_id: { type: Integer },
 };
