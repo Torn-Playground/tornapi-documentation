47a85c40d50ade16859ef9686349a11a8ff374eb
Kobe De Peuter <kobedepeuter@outlook.com>
2023-07-09 17:37:28 +0200

    Update common-params.ts

--- src/api-schema/common-params.ts
+++ src/api-schema/common-params.ts
@@ -2,27 +2,26 @@ import { Param } from "./schema.types";
 
 const TIME_FROM: Param = {
     name: "from",
-    description: "Limits results to have their timestamp after or on this timestamp.",
+    description: "Limits results to have their timestamp after or on this timestamp."
 };
 const TIME_TO: Param = {
-    name: "to",
-    description: "Limits results to have their timestamp before or on this timestamp.",
+    name: "to", description: "Limits results to have their timestamp before or on this timestamp.",
 };
 const TIME_TO_WITH_FROM: Param = {
     name: "to",
-    description: "Limits results to have their timestamp before or on this timestamp. Only available when also using 'from'.",
+description: "Limits results to have their timestamp before or on this timestamp. Only available when also using 'from'.",
 };
 
 const LIMIT: Param = {
-    name: "limit",
-    description: "Limits amount of results. Amount can't be above the default amount, will use default amount otherwise.",
+name: "limit",
+description: "Limits amount of results. Amount can't be above the default amount, will use default amount otherwise.",
 };
 
 const SORT: Param = {
     name: "sort",
-    description: "Sort your results. Default will be descending.",
+    description: "Sort your results. Default will be descending."  ,
     options: {
-        values: ["asc", "desc"],
+        values: ["asc" , "desc" ],
     },
 };
 
