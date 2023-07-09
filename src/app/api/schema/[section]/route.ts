51133cd971786e864fabe75b7aad1c3841fb9340
DeKleineKobini <kobedepeuter@outlook.com>
2023-07-09 17:19:25 +0200

    Properly check if something is a section.

--- src/app/api/schema/[section]/route.ts
+++ src/app/api/schema/[section]/route.ts
@@ -1,8 +1,8 @@
 import { NextResponse } from 'next/server';
-import { schema } from '@/api-schema/data';
+import { isSection, schema } from '@/api-schema/data';
 
 export async function GET(request: Request, { params: { section } }: { params: { section: string } }) {
-  if (schema[section]) {
+  if (isSection(section)) {
     return NextResponse.json(schema[section]);
   } else {
     return NextResponse.json({ error: 'Not found' }, { status: 404 });
