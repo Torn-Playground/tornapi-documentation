3d778a9bec9eec410fe8987de55882e121c56179
Kobe De Peuter <kobedepeuter@outlook.com>
2023-07-10 01:14:27 +0200

    API endpoint for schema access (#100)
    * Added /api route, exporting api schemas programmatically
    
    (cherry picked from commit 557a824ec2b0ef62c29c783244f8b76f04820511)
    
    * Changed to isSection
    
    (cherry picked from commit cd7d596224d03162f26858c82df3901ff025ccd0)
    
    * Move the API endpoints.
    
    * Document the API endpoints.
    
    ---------
    
    Co-authored-by: Sajjad Heydari <mcshemail@gmail.com>

---
+++ src/app/api/sections/route.ts
@@ -0,0 +1,6 @@
+import { NextResponse } from "next/server";
+import { schema } from "@/api-schema/data";
+
+export async function GET(request: Request) {
+    return NextResponse.json({ sections: Object.keys(schema) });
+}
