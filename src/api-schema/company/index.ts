644e41b0fab62697d31a45a2001797213d82fba7
DeKleineKobini <kobedepeuter@outlook.com>
2023-09-01 11:49:42 +0200

    Update schema for company/news and remove company/newsfull.

--- src/api-schema/company/index.ts
+++ src/api-schema/company/index.ts
@@ -4,7 +4,6 @@ import CompaniesSelection from "@/api-schema/company/companies";
 import DetailedSelection from "@/api-schema/company/detailed";
 import EmployeesSelection from "@/api-schema/company/employees";
 import NewsSelection from "@/api-schema/company/news";
-import NewsFullSelection from "@/api-schema/company/newsfull";
 import ProfileSelection from "@/api-schema/company/profile";
 import StockSelection from "@/api-schema/company/stock";
 import { Section, Selection } from "@/api-schema/schema.types";
@@ -16,7 +15,6 @@ const selections: Selection[] = [
     EmployeesSelection,
     LookupSelection,
     NewsSelection,
-    NewsFullSelection,
     ProfileSelection,
     StockSelection,
     TimestampSelection,
