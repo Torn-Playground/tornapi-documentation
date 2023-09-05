c6d00469cc28d1963ff778362f1157c5c1dc5f33
Kobe De Peuter <kobedepeuter@outlook.com>
2023-09-05 16:18:32 +0200

    Implement existing crimes 2.0 selections. (#130)

---
+++ src/api-schema/torn/searchforcash.ts
@@ -0,0 +1,34 @@
+import { Number, String } from "@/api-schema/common-types";
+import { fromStructure, Schema, Selection, Structure } from "@/api-schema/schema.types";
+
+const searchForCashSubcrimeStructure: Structure = {
+    id: "sfc_subcrime",
+    name: "Search for Cash Subcrime",
+    schema: {
+        title: { type: String },
+        percentage: { type: Number },
+    },
+};
+const searchForCashStructure: Structure = {
+    id: "searchforcash",
+    name: "Search for Cash",
+    schema: {
+        "<subcrime name>": fromStructure(searchForCashSubcrimeStructure),
+    },
+};
+const structures = [searchForCashStructure, searchForCashSubcrimeStructure];
+
+const schema: Schema = {
+    searchforcash: fromStructure(searchForCashStructure),
+};
+
+const SearchForCashSelection: Selection = {
+    name: "searchforcash",
+    description: "Check Search for Cash crime status.",
+    access: "public",
+    schema,
+    structures,
+    id: { optional: false },
+};
+
+export default SearchForCashSelection;
