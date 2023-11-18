b64bf5ad453d80c3e74635b19de3e1dc672b9624
Kobe De Peuter <kobedepeuter@outlook.com>
2023-11-18 18:57:39 +0100

    Update user/inventory to reflect it being discontinued.

--- src/api-schema/user/inventory.ts
+++ src/api-schema/user/inventory.ts
@@ -1,31 +1,17 @@
-import { Integer, NumberBoolean, String } from "@/api-schema/common-types";
-import { fromStructure, Schema, Selection, Structure } from "@/api-schema/schema.types";
-
-const inventoryStructure: Structure = {
-    id: "inventory_item",
-    name: "Inventory Item",
-    schema: {
-        ID: { type: Integer },
-        UID: { type: Integer, nullable: true, extra: "Only present on weapons and armor." },
-        name: { type: String },
-        type: { type: String },
-        equipped: { type: NumberBoolean },
-        market_price: { type: Integer },
-        quantity: { type: Integer },
-    },
-};
-const structures = [inventoryStructure];
+import { String } from "@/api-schema/common-types";
+import { Schema, Selection } from "@/api-schema/schema.types";
 
 const schema: Schema = {
-    inventory: fromStructure(inventoryStructure, { array: true }),
+    inventory: { type: String, description: '"The inventory selection is no longer available"' },
 };
 
 const InventorySelection: Selection = {
     name: "inventory",
-    description: "Get all items that are currently in your inventory.",
+    description: "Discontinued; previously got all items that were in your inventory.",
+    warning: "Should not be used, as it's no longer providing the information.",
     access: "minimal",
     schema,
-    structures,
+    structures: [],
     id: { optional: false },
     params: [],
 };
