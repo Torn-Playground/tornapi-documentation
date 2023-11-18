844289acd3df3891bcc4ae74bfa3bbe005daa307
Kobe De Peuter <kobedepeuter@outlook.com>
2023-11-18 19:03:31 +0100

    Update schema for company/news and remove company/newsfull. (#131)

--- src/api-schema/company/news.ts
+++ src/api-schema/company/news.ts
@@ -1,4 +1,4 @@
-import { LIMIT } from "@/api-schema/common-params";
+import { LIMIT, TIME_FROM, TIME_TO } from "@/api-schema/common-params";
 import { fromStructure, Schema, Selection } from "@/api-schema/schema.types";
 import { newsMapStructure, newsStructure } from "@/api-schema/shared/news";
 
@@ -8,14 +8,14 @@ const schema: Schema = { news: fromStructure(newsMapStructure) };
 
 const NewsSelection: Selection = {
     name: "news",
-    description: "Get news entries. Only available for directors.",
+    description: "Get last 25, or 100 when providing the 'from' parameter, news entries. Only available for directors.",
     access: "limited",
     schema,
     structures,
     id: {
         optional: false,
     },
-    params: [LIMIT],
+    params: [TIME_FROM, TIME_TO, LIMIT],
 };
 
 export default NewsSelection;
