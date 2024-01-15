0fa91da6fdd01af9b984e7336c66fa4d3c375ec2
Kobe De Peuter <kobedepeuter@outlook.com>
2024-01-15 13:36:57 +0100

    Remove duplicate code.

--- src/components/try-it/url-selector/url-utilities.ts
+++ src/components/try-it/url-selector/url-utilities.ts
@@ -18,12 +18,9 @@ export function createApiUrl(key: string, section: SectionType | "", id: string,
 
     const url = new URL(`https://api.torn.com/${section}/${id}`);
     const { searchParams } = url;
-    if (selections.length > 0) searchParams.append("selections", selections.join(","));
-    params.forEach((p) => searchParams.append(p.param, p.value));
-    if (comment !== "") searchParams.append("comment", comment);
     searchParams.append("key", key);
 
-    return decodeURIComponent(url.toString());
+    return extendLink(url, selections, params, comment);
 }
 
 export function createShareUrl(section: SectionType | "", id: string, selections: string[], comment: string, params: ParamInput[]): string {
@@ -35,6 +32,13 @@ export function createShareUrl(section: SectionType | "", id: string, selections
     const { searchParams } = url;
     searchParams.append("section", section);
     if (id) searchParams.append("id", id);
+
+    return extendLink(url, selections, params, comment);
+}
+
+function extendLink(url: URL, selections: string[], params: ParamInput[], comment: string): string {
+    const { searchParams } = url;
+
     if (selections.length > 0) searchParams.append("selections", selections.join(","));
     params.forEach((p) => searchParams.append(p.param, p.value));
     if (comment !== "") searchParams.append("comment", comment);
