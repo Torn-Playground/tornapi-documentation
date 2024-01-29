aa625000fb4de33410b7ed149527957689a18c4f
Sashank999 <bandi.rao999@gmail.com>
2024-01-29 21:41:22 +0530

    Use another JSON library.

--- src/components/try-it/call-response/CallResponse.tsx
+++ src/components/try-it/call-response/CallResponse.tsx
@@ -5,7 +5,10 @@ import { useCalls } from "@/components/try-it/CallContext";
 import CopyButton from "@/components/try-it/copy-button/CopyButton";
 import { THEME_DARK, THEME_LIGHT } from "@/components/global/theme-selector/theme-utilities";
 
-import ReactJson from '@microlink/react-json-view'
+import JsonView from '@uiw/react-json-view';
+import { nordTheme } from '@uiw/react-json-view/nord';
+import { githubLightTheme } from '@uiw/react-json-view/githubLight';
+import { TriangleSolidArrow } from '@uiw/react-json-view/triangle-solid-arrow';
 
 export default function CallResponse() {
     const calls = useCalls();
@@ -25,9 +28,13 @@ export default function CallResponse() {
                                 <label className="collapse-title text-xl font-medium break-all break-words" htmlFor={`response-${response.timestamp}`}>
                                     {response.url}
                                 </label>
-                                <div className="collapse-content prose max-w-none">
+                                <div className="collapse-content prose max-w-none text-l">
                                     <CopyButton></CopyButton>
-                                    <ReactJson src={response.data} name={false} theme={theme === THEME_DARK ? "ocean" : "monokai"} iconStyle="circle" />
+                                    <JsonView value={response.data} style={{...(theme === THEME_DARK ? nordTheme : githubLightTheme), ...{ "font-size": "1rem" }}} indentWidth={25} >
+                                        <JsonView.Arrow>
+                                            <TriangleSolidArrow />
+                                        </JsonView.Arrow>
+                                    </JsonView>
                                 </div>
                             </div>
                         ))}
