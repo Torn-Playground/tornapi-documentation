28ac0ba27b4b992f9498e51e6cc95b8787d6e15b
Kobe De Peuter <kobedepeuter@outlook.com>
2024-02-09 19:16:05 +0100

    Reflect user skills being a number now, and add missing skills.

--- src/api-schema/user/skills.ts
+++ src/api-schema/user/skills.ts
@@ -1,12 +1,20 @@
-import { Integer, String } from "@/api-schema/common-types";
+import { Integer, Number } from "@/api-schema/common-types";
 import { Schema, Selection } from "@/api-schema/schema.types";
 
 const schema: Schema = {
-    reviving: { type: String, extra: "in Number format, but it's a string" },
-    search_for_cash: { type: String, nullable: true, extra: "in Number format, but it's a string; Only present for players that migrated to crimes 2.0." },
-    hunting: { type: String, extra: "in Number format, but it's a string" },
-    bootlegging: { type: String, nullable: true, extra: "in Number format, but it's a string; Only present for players that migrated to crimes 2.0." },
-    racing: { type: String, extra: "in Number format, but it's a string" },
+    bootlegging: { type: Number, nullable: true, extra: "Only present for players that migrated to crimes 2.0." },
+    burglary: { type: Number, nullable: true, extra: "Only present for players that migrated to crimes 2.0." },
+    card_skimming: { type: Number, nullable: true, extra: "Only present for players that migrated to crimes 2.0." },
+    cracking: { type: Number, nullable: true, extra: "Only present for players that migrated to crimes 2.0." },
+    disposal: { type: Number, nullable: true, extra: "Only present for players that migrated to crimes 2.0." },
+    graffiti: { type: Number, nullable: true, extra: "Only present for players that migrated to crimes 2.0." },
+    hustling: { type: Number, nullable: true, extra: "Only present for players that migrated to crimes 2.0." },
+    pickpocketing: { type: Number, nullable: true, extra: "Only present for players that migrated to crimes 2.0." },
+    reviving: { type: Number },
+    search_for_cash: { type: Number, nullable: true, extra: "Only present for players that migrated to crimes 2.0." },
+    shoplifting: { type: Number, nullable: true, extra: "Only present for players that migrated to crimes 2.0." },
+    hunting: { type: Number },
+    racing: { type: Number },
     player_id: { type: Integer },
 };
 
