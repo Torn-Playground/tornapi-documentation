efcc5672644ec95052fb00e7d78d941238f108d4
Kobe De Peuter <kobedepeuter@outlook.com>
2024-02-11 00:27:36 +0100

    Include missing skills in the user/skills schema. (#164)

--- src/api-schema/user/skills.ts
+++ src/api-schema/user/skills.ts
@@ -2,10 +2,18 @@ import { Integer, String } from "@/api-schema/common-types";
 import { Schema, Selection } from "@/api-schema/schema.types";
 
 const schema: Schema = {
-    reviving: { type: String, extra: "in Number format, but it's a string" },
-    search_for_cash: { type: String, nullable: true, extra: "in Number format, but it's a string; Only present for players that migrated to crimes 2.0." },
-    hunting: { type: String, extra: "in Number format, but it's a string" },
     bootlegging: { type: String, nullable: true, extra: "in Number format, but it's a string; Only present for players that migrated to crimes 2.0." },
+    burglary: { type: String, nullable: true, extra: "in Number format, Only present for players that migrated to crimes 2.0." },
+    card_skimming: { type: String, nullable: true, extra: "in Number format, Only present for players that migrated to crimes 2.0." },
+    cracking: { type: String, nullable: true, extra: "in Number format, Only present for players that migrated to crimes 2.0." },
+    disposal: { type: String, nullable: true, extra: "in Number format, Only present for players that migrated to crimes 2.0." },
+    graffiti: { type: String, nullable: true, extra: "in Number format, Only present for players that migrated to crimes 2.0." },
+    hustling: { type: String, nullable: true, extra: "in Number format, Only present for players that migrated to crimes 2.0." },
+    pickpocketing: { type: String, nullable: true, extra: "in Number format, Only present for players that migrated to crimes 2.0." },
+    reviving: { type: String, extra: "in Number format, but it's a string" },
+    search_for_cash: { type: String, nullable: true, extra: "in Number format, Only present for players that migrated to crimes 2.0." },
+    shoplifting: { type: String, nullable: true, extra: "in Number format, Only present for players that migrated to crimes 2.0." },
+    hunting: { type: String, extra: "in Number format, but it's a string" },
     racing: { type: String, extra: "in Number format, but it's a string" },
     player_id: { type: Integer },
 };
